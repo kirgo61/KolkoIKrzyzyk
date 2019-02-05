86dc1983739f4a7ff8949137da6def8ca720457a
kirgo61 <40340548+kirgo61@users.noreply.github.com>
2019-02-05 19:05:29 +0100

    KolkoIKrzyzyk

---
+++ KolkoIKrzyzyk.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KolkoIKrzyzyk.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KolkoIKrzyzyk.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("526a395d-8753-4bb3-915e-eb5ba5e737da")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ KolkoIKrzyzyk.Test/Testy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KolkoIKrzyzyk;
+
+namespace KolkoIKrzyzyk.Test
+{
+    [TestClass]
+    public class Testy
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

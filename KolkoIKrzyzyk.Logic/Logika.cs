fb9e57424b1d38d66b55e04d5feeb5061b337509
kirgo61 <40340548+kirgo61@users.noreply.github.com>
2019-02-09 23:49:40 +0100

    UI update.

--- KolkoIKrzyzyk.Logic/Logika.cs
+++ KolkoIKrzyzyk.Logic/Logika.cs
@@ -8,7 +8,77 @@ namespace KolkoIKrzyzyk.Logic
 {
     public class Logika
     {
-      
-        
+        public Logika() { }
+        private int amountOfMoves = 0;
+        //liczenie ruchów
+        public int AmountOfMoves
+        {
+            get { return amountOfMoves; }
+            set { amountOfMoves = value; }
+        }
+
+        private int currentPlayer = 1;
+        public int CurrentPlayer
+        {
+            get { return currentPlayer; }
+            set { currentPlayer = value; }
+        }
+        //bool wygrana albo nie
+        private bool gameOver;
+        public bool GameOver
+        {
+            get { return gameOver; }
+            set { gameOver = value; }
+        }
+        public bool IsPlayerOneTurn
+        {
+            get { return AmountOfMoves % 2 == 0; }
+        }
+        private int[] pressedButtons = new int[25];
+        public int[] PressedButtons
+        {
+            get { return pressedButtons; }
+            set { pressedButtons = value; }
+        }
+        private bool WinningCombinationFound(int left1, int left2, int middle, int right2, int right1)
+        {
+            return (PressedButtons[left1] == CurrentPlayer &&
+                PressedButtons[left2] == CurrentPlayer &&
+                PressedButtons[middle] == CurrentPlayer &&
+                PressedButtons[right2] == CurrentPlayer &&
+                PressedButtons[right1] == CurrentPlayer);
+        }
+        private bool GameWinnerFound()
+        {
+            //poziomo
+            if (WinningCombinationFound(0, 1, 2, 3, 4)) return true;
+            if (WinningCombinationFound(5, 6, 7, 8, 9)) return true;
+            if (WinningCombinationFound(10, 11, 12, 13, 14)) return true;
+            if (WinningCombinationFound(15, 16, 17, 18, 19)) return true;
+            if (WinningCombinationFound(20, 21, 22, 23, 24)) return true;
+            //pionowo
+            if (WinningCombinationFound(0, 5, 10, 15, 20)) return true;
+            if (WinningCombinationFound(1, 6, 11, 16, 21)) return true;
+            if (WinningCombinationFound(2, 7, 12, 17, 22)) return true;
+            if (WinningCombinationFound(3, 8, 13, 18, 23)) return true;
+            if (WinningCombinationFound(4, 9, 22, 23, 24)) return true;
+            //skos
+            if (WinningCombinationFound(0, 6, 12, 18, 24)) return true;
+            if (WinningCombinationFound(4, 8, 12, 16, 20)) return true;
+            return false;
+        }
+        public void CheckForGameWinner()
+        {
+            GameOver = GameWinnerFound();
+        }
+        //restart
+        public void ResetGame()
+        {
+            AmountOfMoves = 0;
+            GameOver = false;
+            CurrentPlayer = 1;
+            PressedButtons = new int[25];
+        }
+
     }
 }
--- KolkoIKrzyzyk/MainWindow.xaml.cs
+++ KolkoIKrzyzyk/MainWindow.xaml.cs
@@ -21,17 +21,132 @@ namespace KolkoIKrzyzyk
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string player1Alert = "Tura Gracza 1";
+        private const string player2Alert = "Tura Gracza 2";
+        private const string draw = "REMIS";
+        private const string markX = "X";
+        private const string markO = "O";
+
+
+        private Logika kolkoIKrzyzykLogika = null;
+        private Logika KolkoIKrzyzykLogika
+        {
+            get
+            {
+                if (kolkoIKrzyzykLogika == null) { kolkoIKrzyzykLogika = new Logika(); }
+                return kolkoIKrzyzykLogika;
+            }
+        }
+
+
+        private void ButtonClick(object sender, MouseButtonEventArgs e)
+        {
+            if (KolkoIKrzyzykLogika.GameOver)
+            {
+                return;
+            }
+            Rectangle click = e.Source as Rectangle;
+            int buttonUid = Convert.ToInt32(click.Uid);
+
+            if (IsValidMove(buttonUid))
+            {
+                UpdateGameUi(click);
+                UpdateGameLogicMap(buttonUid);
+                KolkoIKrzyzykLogika.AmountOfMoves++;
+                if (KolkoIKrzyzykLogika.AmountOfMoves >= 9)
+                {
+                    CheckForGameWinner();
+                    CheckForGameTie();
+                }
+                KolkoIKrzyzykLogika.CurrentPlayer = KolkoIKrzyzykLogika.IsPlayerOneTurn ? 1 : 2;
+            }
+        }
+
+
+        private void UpdateGameLabelForNextPlayer(string labelMessage, Brush color)
+        {
+            Info.Content = labelMessage;
+            Info.Foreground = color;
+        }
+
+
         public MainWindow()
         {
             InitializeComponent();
+            UpdateGameLabelForNextPlayer(player1Alert, Brushes.Red);
         }
-        private void ButtonClick(object sender, MouseButtonEventArgs e)
-        {
 
-        }
+        /// <param name="sender"></param>
+		/// <param name="e"></param>
         private void Btn_Reset_MouseLeftButtonDown(object sender, RoutedEventArgs e)
         {
+            foreach (Rectangle gamePieces in Container.Children.OfType<Rectangle>())
+            {
+                gamePieces.Fill = new SolidColorBrush(Colors.Wheat);
+            }
+            KolkoIKrzyzykLogika.ResetGame();
+            UpdateGameLabelForNextPlayer(player1Alert, Brushes.Red);
+        }
 
+
+        private void CheckForGameWinner()
+        {
+            KolkoIKrzyzykLogika.CheckForGameWinner();
+
+            if (KolkoIKrzyzykLogika.GameOver)
+            {
+                UpdateGameLabelForNextPlayer(string.Format("Gracz {0} wygrał!", KolkoIKrzyzykLogika.CurrentPlayer), Brushes.DarkGoldenrod);
+            }
+        }
+
+
+        private void CheckForGameTie()
+        {
+            if (KolkoIKrzyzykLogika.AmountOfMoves >= 25 && !KolkoIKrzyzykLogika.GameOver)
+            {
+                UpdateGameLabelForNextPlayer(draw, Brushes.Black);
+                KolkoIKrzyzykLogika.GameOver = true;
+            }
+        }
+
+
+        private bool IsValidMove(int clickedRectangleIndex)
+        {
+            if (KolkoIKrzyzykLogika.PressedButtons[clickedRectangleIndex] == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        private void UpdateGameUi(Rectangle clickedButton)
+        {
+            if (KolkoIKrzyzykLogika.IsPlayerOneTurn)
+            {
+                UpdateButtonFill(clickedButton, markX, new SolidColorBrush(Colors.Red));
+                UpdateGameLabelForNextPlayer(player2Alert, Brushes.Blue);
+            }
+            else
+            {
+                UpdateButtonFill(clickedButton, markO, new SolidColorBrush(Colors.Blue));
+                UpdateGameLabelForNextPlayer(player1Alert, Brushes.Red);
+            }
+        }
+        private void UpdateButtonFill(Rectangle clickedButton, string playerSymbol, Brush color)
+        {
+            if (clickedButton != null)
+            {
+                TextBlock tb = new TextBlock();
+                tb.FontSize = 72;
+                tb.Background = color;
+                tb.Text = playerSymbol;
+                BitmapCacheBrush bcb = new BitmapCacheBrush(tb);
+                clickedButton.Fill = bcb;
+            }
+        }
+        private void UpdateGameLogicMap(int arrayIndex)
+        {
+            int mapValue = KolkoIKrzyzykLogika.CurrentPlayer;
+            KolkoIKrzyzykLogika.PressedButtons[arrayIndex] = mapValue;
         }
     }
 }

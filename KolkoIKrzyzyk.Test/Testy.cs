6156d8f38dbd236c33960adc2e45d2f5b16991ed
kirgo61 <40340548+kirgo61@users.noreply.github.com>
2019-02-10 18:02:54 +0100

    Finalna wersja + dodanie instalatora.

--- KolkoIKrzyzyk.Test/Testy.cs
+++ KolkoIKrzyzyk.Test/Testy.cs
@@ -7,9 +7,37 @@ namespace KolkoIKrzyzyk.Test
     [TestClass]
     public class Testy
     {
+
         [TestMethod]
-        public void TestMethod1()
+        public void CheckForGameWinner_NoWinner_ReturnsFalse()
         {
+            // Arrange
+            Logika gameLogic = new Logika();
+            gameLogic.CurrentPlayer = 1;
+            gameLogic.PressedButtons = new int[] { 1, 2, 1, 2, 1, 2, 1, 2, 2, 1, 1, 1, 2, 2, 1, 2, 2, 1, 1, 1, 1, 2, 1, 2, 2 };
+            bool gameWon = false;
+
+            // Act
+            gameLogic.CheckForGameWinner();
+
+            // Assert
+            Assert.AreEqual(gameWon, gameLogic.GameOver);
+        }
+
+        [TestMethod]
+        public void CheckForGameWinner_ValidWinner_ReturnsTrue()
+        {
+            // Arrange
+            Logika gameLogic = new Logika();
+            gameLogic.CurrentPlayer = 1;
+            gameLogic.PressedButtons = new int[] { 1, 1, 2, 0, 0, 0, 1, 2, 1, 0, 2, 1, 2, 2, 1, 2, 1, 2, 1, 2, 2, 1, 1, 2, 1 };
+            bool gameWon = true;
+
+            // Act
+            gameLogic.CheckForGameWinner();
+
+            // Assert
+            Assert.AreEqual(gameWon, gameLogic.GameOver);
         }
     }
 }
